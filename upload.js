e7eb9b798d8e5c85ed59e0182163032a9da09685
matcsp <apbuildpro@gmail.com>
2025-07-30 22:06:44 +0800

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,14 @@
+# 1. Initialize Git (if not already done)
+git init
+
+# 2. Add your new script file
+git add upload.js
+
+# 3. Commit with a message
+git commit -m "Add Cloudflare image upload script"
+
+# 4. Add your GitHub repo as remote
+git remote add origin https://github.com/YOUR_USERNAME/YOUR_REPO.git
+
+# 5. Push to GitHub
+git push -u origin main
